8c279aaa63487d456c0e55f94c835682b02f4b70
Michael Brennan <brennan.brisad@gmail.com>
2015-03-18 07:28:31 +0100

    Connect websockets

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask
-from flask.ext.socketio import SocketIO
+from flask import Flask, render_template
+from flask.ext.socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'not so secret'
 socketio = SocketIO(app)
 
 @app.route("/")
-def main():
-    return ""
+def index():
+    return render_template('index.html')
 
+@socketio.on('my event')
+def test_message(message):
+    emit('my reponse', {'data': 'got it!'})
 
 if __name__ == '__main__':
     socketio.run(app)
