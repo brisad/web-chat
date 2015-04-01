6f6bc54f0bdc9481c51dd0c3b407c7556016b6fb
Michael Brennan <brennan.brisad@gmail.com>
2015-04-01 23:16:59 +0200

    Add basic registration messages

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from flask import Flask, render_template
 from flask.ext.socketio import SocketIO, emit
 
@@ -9,9 +11,27 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
-@socketio.on('my event')
-def test_message(message):
-    emit('my reponse', {'data': 'got it!'})
+registered_users = set()
+
+@socketio.on('register')
+def register_user(username):
+    if username in registered_users:
+        emit('registered', False)
+    else:
+        registered_users.add(username)
+        emit('registered', True)
+
+    print("Users: " + str(registered_users))
+
+@socketio.on('unregister')
+def unregister_user(username):
+    try:
+        registered_users.remove(username)
+    except KeyError:
+        print("Unknown user '{}' tried to unregister".format(username))
+
+    print("Users: " + str(registered_users))
+
 
 if __name__ == '__main__':
     socketio.run(app)
--- static/messages.js
+++ static/messages.js
@@ -1,4 +1,10 @@
+var userName = "Guest" + parseInt(Math.random() * 10);
+
 var socket = io.connect('http://' + document.domain + ':' + location.port);
 socket.on('connect', function () {
-  socket.emit('my event', {data: 'I\'m connected!'});
+  socket.emit('register', userName);
+});
+
+socket.on('registered', function (success) {
+  console.log("Registered: " + success);
 });
