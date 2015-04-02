d170e5a1785a8d4905d2ae49c7c8251df5f05b45
Michael Brennan <brennan.brisad@gmail.com>
2015-04-02 07:26:16 +0200

    Only unregister if successfully registered

--- static/messages.js
+++ static/messages.js
@@ -1,16 +1,20 @@
-var userName = "Guest" + parseInt(Math.random() * 10);
+var username = "Guest" + parseInt(Math.random() * 10);
+var registered = false;
 
 var socket = io.connect('http://' + document.domain + ':' + location.port);
 socket.on('connect', function () {
   // Add handler for performing clean up at exit
   $(window).bind("beforeunload", function() {
-    socket.emit('unregister', userName);
+    if (registered) {
+      socket.emit('unregister', username);
+    }
     socket.disconnect();
   });
 
-  socket.emit('register', userName);
+  socket.emit('register', username);
 });
 
 socket.on('registered', function (success) {
   console.log("Registered: " + success);
+  registered = success;
 });
