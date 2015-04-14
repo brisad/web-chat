483fb5e581b0b7856787aaa57ce223600dd7b10f
Michael Brennan <brennan.brisad@gmail.com>
2015-04-14 17:44:45 +0200

    Add gulp-size

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@
 
 var gulp = require('gulp');
 var react = require('gulp-react');
+var size = require('gulp-size');
 
 gulp.task('default', function () {
   return gulp.src('./static/chat.jsx')
     .pipe(react())
-    .pipe(gulp.dest('./static'));
+    .pipe(size())
+    .pipe(gulp.dest('./static'))
 });
